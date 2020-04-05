79164f53395bbddf6cfa6747fd3568e7e0296657
unknown <howwwwwhy@gmail.com>
2020-04-05 12:57:10 +0900

    implement functions (Part 4-8 in function components version)

--- App.js
+++ App.js
@@ -7,58 +7,129 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-const NumberPad = () => {
+/*
+import {YellowBox} from 'react-native';
+YellowBox.ignoreWarnings(['Warning: ...']);
+*/
+const NumberPad = (props) => {
   let rows = [];
   let nums = [
     [7, 8, 9],
     [4, 5, 6],
     [1, 2, 3],
-    ['+/-', 0, '='],
+    ['.', 0, '='],
   ];
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < nums.length; i++) {
     let row = [];
-    for (let j = 0; j < 3; j++) {
+    for (let j = 0; j < nums[0].length; j++) {
       row.push(
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.textButton} key={i * 3 + j}>
-            {nums[i][j]}
-          </Text>
+        <TouchableOpacity
+          key={nums[i][j]}
+          onPress={() => props.onPress(nums[i][j])}
+          style={styles.button}>
+          <Text style={styles.textButton}>{nums[i][j]}</Text>
         </TouchableOpacity>,
       );
     }
-    rows.push(<View style={styles.row}>{row}</View>);
+    rows.push(
+      <View key={i} style={styles.row}>
+        {row}
+      </View>,
+    );
   }
   return rows;
 };
 
-const OperationPad = () => {
-  let operations = ['÷', '×', '－', '＋'];
+const OperationPad = (props) => {
   let rows = [];
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < props.operations.length; i++) {
     rows.push(
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.textButton}>{operations[i]}</Text>
+      <TouchableOpacity
+        key={props.operations[i]}
+        onPress={() => props.onPress(props.operations[i])}
+        style={styles.button}>
+        <Text style={styles.textButton}>{props.operations[i]}</Text>
       </TouchableOpacity>,
     );
   }
   return rows;
 };
 const App = () => {
+  const [calculation, setCalculation] = useState('');
+  const [result, setResult] = useState('');
+  const operations = ['DEL', '/', '*', '-', '+'];
+
+  const calculationResult = () => {
+    // Brackets -> of -> division -> mult -> add -> substract
+    setResult(eval(calculation));
+  };
+
+  const isValid = () => {
+    console.log(calculation.slice(-1));
+    if (operations.indexOf(calculation.slice(-1)) > 0) return false;
+    else return true;
+  };
+
+  const buttonPressed = (text) => {
+    console.log(text);
+
+    const lastChar = calculation.split('').pop();
+
+    switch (text) {
+      case '=':
+        isValid() && calculationResult();
+        break;
+      case 'DEL':
+        setCalculation(calculation.slice(0, -1));
+        break;
+      case '/':
+        if (operations.indexOf(lastChar) > 0) {
+          setCalculation(calculation.slice(0, -1) + text);
+        } else if (calculation != '') {
+          setCalculation((prevState) => prevState + text);
+        }
+        break;
+      case '*':
+        if (operations.indexOf(lastChar) > 0) {
+          setCalculation(calculation.slice(0, -1) + text);
+        } else if (calculation != '') {
+          setCalculation((prevState) => prevState + text);
+        }
+        break;
+      case '-':
+        if (operations.indexOf(lastChar) > 0) {
+          setCalculation(calculation.slice(0, -1) + text);
+        } else if (calculation != '') {
+          setCalculation((prevState) => prevState + text);
+        }
+        break;
+      case '+':
+        if (operations.indexOf(lastChar) > 0) {
+          setCalculation(calculation.slice(0, -1) + text);
+        } else if (calculation != '') {
+          setCalculation((prevState) => prevState + text);
+        }
+        break;
+      default:
+        setCalculation((prevState) => prevState + text);
+    }
+  };
+
   return (
     <>
       <View style={styles.container}>
         <View style={styles.calculation}>
-          <Text style={styles.textCalculation}>11x11</Text>
+          <Text style={styles.textCalculation}>{calculation}</Text>
         </View>
         <View style={styles.result}>
-          <Text style={styles.textResult}>121</Text>
+          <Text style={styles.textResult}>{result}</Text>
         </View>
         <View style={styles.buttons}>
           <View style={styles.numbers}>
-            <NumberPad />
+            <NumberPad onPress={buttonPressed} />
           </View>
           <View style={styles.operations}>
-            <OperationPad />
+            <OperationPad onPress={buttonPressed} operations={operations} />
           </View>
         </View>
       </View>
@@ -72,13 +143,13 @@ const styles = StyleSheet.create({
   },
   calculation: {
     flex: 2,
-    backgroundColor: 'green',
+    backgroundColor: '#FFC312',
     justifyContent: 'center',
     alignItems: 'flex-end',
   },
   result: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#6ab04c',
     justifyContent: 'center',
     alignItems: 'flex-end',
   },
@@ -88,11 +159,11 @@ const styles = StyleSheet.create({
   },
   numbers: {
     flex: 3,
-    backgroundColor: 'yellow',
+    backgroundColor: '#badc58',
   },
   operations: {
     flex: 1,
-    backgroundColor: 'grey',
+    backgroundColor: '#c7ecee',
     justifyContent: 'space-around',
   },
   row: {
@@ -104,7 +175,7 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   textResult: {
-    fontSize: 24,
+    fontSize: 30,
     color: 'white',
   },
   button: {
@@ -114,6 +185,7 @@ const styles = StyleSheet.create({
   },
   textButton: {
     fontSize: 30,
+    fontWeight: 'bold',
   },
 });
 
